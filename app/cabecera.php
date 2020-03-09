624eac2c86e4dca21c73b2b5452d2b1595dbc7af
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-09 11:57:14 +0100

    cabecera y ficheros esquema

---
+++ app/cabecera.php
@@ -0,0 +1,14 @@
+<div class="logo"><a href="index.php"><img src="img/logo.jpg" alt="Logo"></a></div>
+<div class="status">
+    <span>Hola user</span>
+    <button type="submit">Login</button>
+</div>
+<nav>
+    <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="catalogo.php">Catalogo</a></li>
+            <li><a href="chat.php">Chat</a></li>
+            <li><a href="perfil.php">Perfil</a></li>
+            <li><a href="about.php">Sobre nosotros</a></li>
+    </ul>
+</nav>
