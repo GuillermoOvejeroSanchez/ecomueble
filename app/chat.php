6a2ef083a7987eae9466a564a9c4c0abf5452074
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-09 12:47:57 +0100

    session persistente en diferentes paginas + css default

--- app/chat.php
+++ app/chat.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require('head.php')?>
     <title>Document</title>
 </head>
 
---
+++ app/head.php
@@ -0,0 +1,5 @@
+
+<?php 
+session_start();
+?>
+<link rel="stylesheet" href="./css/default.css">
