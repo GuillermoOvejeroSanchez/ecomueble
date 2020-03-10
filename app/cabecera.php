893e86279689ce15e9db94dfc1d6704448867c2d
Noeliac <ncalde01@ucm.es>
2020-03-10 11:21:20 +0100

    poquito a poquito

--- app/cabecera.php
+++ app/cabecera.php
@@ -1,15 +1,23 @@
 <div class="logo"><a href="index.php"><img src="img/logo.jpg" alt="Logo"></a></div>
 <div class="status">
-    <form action="login_form.php" method="post">
+   <!-- <form action="login_form.php" method="post">-->
     <?php 
-    if(isset($_SESSION['user'])){
+    /*if(isset($_SESSION['user'])){
         $user = $_SESSION['user'];
         echo '<span>'. $user .'</span>
-         <button type="submit" name="logout">Logout</button>';
+        <button type="submit" name="logout">Logout</button>';
     }else{
         echo '
         <button type="submit" name="login">Login</button>
         <button type="submit" name="registrar">Registrar</button>';
+    }*/
+    if(isset($_SESSION['login'])) {
+        Echo "<button type='/submit'/ name='/logout'/>Logout</button>'";
+    }
+    else {
+        echo '
+        <a href="./login_form.php">Login</a>
+        <a href="./login_form.php">Registrar</a>'; //./ porque luego no estaremos en local:)
     }
     ?>
     </form>
--- app/login_form.php
+++ app/login_form.php
@@ -1,9 +1,17 @@
+<?php session_start(); ?>
+
+
 <?php
 
 //Login prueba, acceder a base de datos segun el caso
-session_start();
+    $username = "username";
+    $password = "password";
+    Echo "<form method='POST' action='./index.php'>";
+    Echo "<div><input type='text' name=$username value='user'/></div>";
+    Echo "<div><input type='text' name=$password value='userpass'/></div>";
+    Echo "<div> <button type='submit' name='submit2' value='Enviar form.'> Entrar  </button></div>";
 
-if(isset($_REQUEST['login'])) {
+/*if(isset($_REQUEST['login'])) {
     $_SESSION['user'] = 'User';
 }
 
@@ -15,7 +23,7 @@ elseif (isset($_REQUEST['logout'])) {
     unset($_SESSION['user']);
     session_destroy();
 }
-
-header("Location: ./index.php");
+*/
+//header("Location: ./index.php"); 
 die();
 ?>
