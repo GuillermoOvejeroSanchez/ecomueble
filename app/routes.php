42f58dedabaa6c44ed65d43e6f1b6b98ba0c28a2
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-15 16:48:49 +0100

    .htacces para no cambiar config xampp

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,11 @@ switch ($request) {
     case '/':
         require __DIR__.
         '/views/home.php';
-        break;
+    break;
     case '/index.php':
         require __DIR__.
         '/views/home.php';
-        break;
+    break;
     case '/about':
         require __DIR__.
         '/views/about.php';
@@ -35,6 +35,10 @@ switch ($request) {
         require __DIR__.
         '/views/perfil.php';
         break;
+        case '/status':
+            require __DIR__.
+            '/status_redirect.php';
+            break;
     default:
         http_response_code(404);
         require __DIR__.
