b293489f543138474ebd27bb64b21c30a99e3b6f
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-15 04:08:02 +0100

    Revert "logger"
    This reverts commit 030c076c7bf8f20f4ebb3a4d247ec549910feb6d.

--- app/views/registrar.php
+++ app/views/registrar.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php //session_start(); 
-    require('./common/head.php')?>
+    <?php require('./common/head.php')?>
     <title>Registrar</title>
 </head>
 
