030c076c7bf8f20f4ebb3a4d247ec549910feb6d
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-15 04:07:48 +0100

    logger

--- app/views/registrar.php
+++ app/views/registrar.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require('./common/head.php')?>
+    <?php //session_start(); 
+    require('./common/head.php')?>
     <title>Registrar</title>
 </head>
 
