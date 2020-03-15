464838614e41a9226b30426cb8e9c50142030e62
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-15 04:14:25 +0100

    del logger

--- app/index.php
+++ app/index.php
@@ -1,5 +1,4 @@
 <?php
 require_once('config.php');
 require('routes.php');
-require('logger.php');
 ?>
--- app/views/registrar.php
+++ app/views/registrar.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php //session_start(); 
+    <?php
     require('./common/head.php')?>
     <title>Registrar</title>
 </head>
