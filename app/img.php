a9e44e595bc6d3eb35abc6416e3f89423fd64336
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2020-03-17 23:56:31 +0100

    Update img.php

--- app/img.php
+++ app/img.php
@@ -1,21 +1,25 @@
 <?php
 
 function saveImg($path, $filename){
-    $target_dir = $path;
-    $newname = $filename;
-    $ext = pathinfo($_FILES["imagen"]["name"],PATHINFO_EXTENSION);
-    $target_file = $target_dir . $newname . '.' .$ext;
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-    // Check if image file is a actual image or fake image
-    $check = getimagesize($_FILES["imagen"]["tmp_name"]);
-    if($check !== false) {
-        $uploadOk = 1;
-        move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file);
-        return $newname. '.' . $ext;
-    } else {
+    if (!file_exists($_FILES['imagen']['tmp_name']) || !is_uploaded_file($_FILES['imagen']['tmp_name'])){
         $uploadOk = 0;
+    }else{
+        $target_dir = $path;
+        $newname = $filename;
+        $ext = pathinfo($_FILES["imagen"]["name"],PATHINFO_EXTENSION);
+        $target_file = $target_dir . $newname . '.' .$ext;
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        
+        // Check if image file is a actual image or fake image
+        $check = getimagesize($_FILES["imagen"]["tmp_name"]);
+        if($check !== false) {
+            $uploadOk = 1;
+            move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file);
+            return $newname. '.' . $ext;
+        } else {
+            $uploadOk = 0;
+        }
     }
 }
 ?>
