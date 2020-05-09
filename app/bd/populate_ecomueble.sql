b6560f26c8de80fcae3128afe9226beab2e81d72
Marta Ranz González <mranz02@ucm.es>
2020-05-09 17:09:21 +0200

    Se me olvidaba cambiar esto

--- app/bd/populate_ecomueble.sql
+++ app/bd/populate_ecomueble.sql
@@ -1,4 +1,4 @@
 ALTER TABLE usuario AUTO_INCREMENT = 1;
 INSERT INTO `usuario` (`idUsuario`, `nombre`, `email`, `telefono`, `password`, `tipoUsuario`, `saldo`, `imagen`) VALUES
-(20, 'wilson', 'guilleov@ucm.es', 684216696, '$2y$10$h681e.VAdpki37HrDiRlF.lG5v9W420s1QAfFxcWjLVjNgleAVqUa', 1, 999, 'wilson.png'),
-(21, 'prueba', 'prueba@prueba.com', 657845213, '$2y$10$A36aJIWPRxz7oEkhZoKzveiYxtmPKqu4eczI/sn8qJYDX3.tfszH6', 1, 999, 'default_profile.jpg');
+(21, 'wilson', 'guilleov@ucm.es', 684216696, '$2y$10$h681e.VAdpki37HrDiRlF.lG5v9W420s1QAfFxcWjLVjNgleAVqUa', 1, 999, 'wilson.png'),
+(22, 'prueba', 'prueba@prueba.com', 657845213, '$2y$10$A36aJIWPRxz7oEkhZoKzveiYxtmPKqu4eczI/sn8qJYDX3.tfszH6', 1, 999, 'default_profile.jpg');
