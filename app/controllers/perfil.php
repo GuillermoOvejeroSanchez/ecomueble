727a2ae83cc5e7fed7a371cb439700f885c3aef6
Víctor Santamaría <vsantama@ucm.es>
2020-05-09 19:50:26 +0200

    Reformateo de código

--- app/controllers/perfil.php
+++ app/controllers/perfil.php
@@ -16,12 +16,16 @@ function logged()
     <div class="perfil"> 
     <form action="status" method="post">
         <?php
-        echo" <table class='per'> <tr> <th class='imagen'> <img src='$imagen' alt='imagen'></th> 
-                            <th class='datos'><p>Nombre: <strong>$user->nombre</strong></p> 
-                                            <p>Email: <strong>$user->email</strong></p>
-                                            <p>Teléfono: <strong>$user->telefono</strong> </p></th>
-              </tr> </table>
-           ";//Imagen de perfil y datos informativos
+        echo"<table class='per'> 
+                <tr> 
+                    <th class='imagen'> <img src='$imagen' alt='imagen'></th> 
+                    <th class='datos'>
+                        <p>Nombre: <strong>$user->nombre</strong></p> 
+                        <p>Email: <strong>$user->email</strong></p>
+                        <p>Teléfono: <strong>$user->telefono</strong> </p>
+                    </th>
+                </tr> 
+            </table>";//Imagen de perfil y datos informativos
 
         echo "<div class='bperfil'><button type='submit' name='subirProducto'>Subir Producto</button>
         <button type='submit' name='editarPerfil'>Editar Perfil</button></div>";//botones
