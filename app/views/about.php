e2126b6992bef915a06b12cc8d5e6ec947ba8a70
Víctor Santamaría <vsantama@ucm.es>
2020-05-10 20:55:20 +0200

    end CSS entrega 3

--- app/views/about.php
+++ app/views/about.php
@@ -13,165 +13,157 @@
     require('./includes/common/cabecera.php');
     ?>
 
-    <nav class='miembros'>
-        <!-- nav-bar de miembros-->
-        <ul>
-            <li><a href="#noe">Noelia Calderón</a></li>
-            <li><a href="#marta">Marta Ranz</a></li>
-            <li><a href="#rebeca">Rebeca Herranz</a></li>
-            <li><a href="#adri">Adrián García</a></li>
-            <li><a href="#victor">Víctor Santamaría</a></li>
-            <li><a href="#guille">Guillermo Ovejero</a></li>
-            <li><a href="#marlon">Marlon Campoverde</a></li>
-        </ul>
-</nav>
+<nav_about>
+    <!-- nav-bar de miembros-->
+    <ul class="nav_bar">
+        <li><a href="#noe">Noelia Calderón</a></li>
+        <li><a href="#marta">Marta Ranz</a></li>
+        <li><a href="#rebeca">Rebeca Herranz</a></li>
+        <li><a href="#adri">Adrián García</a></li>
+        <li><a href="#victor">Víctor Santamaría</a></li>
+        <li><a href="#guille">Guillermo Ovejero</a></li>
+    </ul>
+</nav_about>
 
-    <h1>Miembros de Ecomueble</h1>
-    <div class="miembro">
-        <div id="noe" class="member_photo">
-            <p>Noelia Calderón</p>
-            <img src="img/Noelia.png" alt="Foto Noe">
+
+    <div class="container_miembros">
+        <p>Noelia Calderón</p>
+        <div class="contenido_miembros">
+
+            <div id="noe" class="perfil">
+                <img src="img/Noelia.png" alt="Foto Noe">
+            </div>
+            <ul>
+                <li>
+                    Jefa de proyecto, además lleva a cabo tareas de desarrollo y diseño. 
+                </li>
+                <li>
+                    Siempre le gustó todo lo relacionado con la tecnología, hecho que le llevó a centrarse en la informática.
+                </li>
+                <li>
+                    Su principal afición es la cocina, especialmente la repostería, siempre disfruta descubriendo y creando recetas con chocolate.
+                </li>
+                <li class="contacto">
+                    Contacto: ncalde01@ucm.es
+                </li>
+            </ul>
         </div>
-        <ul>
-            <li>
-                Jefa de proyecto, además lleva a cabo tareas de desarrollo y diseño. 
-            </li>
-            <li>
-                Siempre le gustó todo lo relacionado con la tecnología, hecho que le llevó a centrarse en la informática.
-            </li>
-            <li>
-                Su principal afición es la cocina, especialmente la repostería, siempre disfruta descubriendo y creando recetas con chocolate.
-            </li>
-            <li class="contacto">
-                Contacto: ncalde01@ucm.es
-            </li>
-        </ul>
+    </div>
+        
+    <div class="container_miembros">
+        <p>Marta Ranz</p>
+        <div class="contenido_miembros">
+            <div id="marta" class="perfil">
+                <img src="img/Marta.png" alt="Foto Marta">
+            </div>
+            <ul>
+                <li>
+                    Encargada de diseño y ayudante de equipo.
+                </li>
+                <li>
+                    Experta en redes sociales, es la enargada de todo lo relativo a la publicidad y el contacto con el público.
+                </li>
+                <li>
+                    Su principal afición son los animales, resalta su gran conciencia contra el maltrato animal y el cuidado del medioambiente.
+                </li>
+                <li class="contacto">
+                    Contacto: mranz02@ucm.es
+                </li>
+            </ul>
+        </div>
     </div>
 
-    <div class="miembro">
-        <div id="marta" class="member_photo">
-            <p>Marta Ranz</p>
-            <img src="img/Marta.png" alt="Foto Marta">
+    <div class="container_miembros">
+        <p>Rebeca Herranz</p>
+        <div class="contenido_miembros">
+            <div id="rebeca" class="perfil">
+                <img src="img/Rebeca.jpg" alt="Foto Rebe">
+            </div>
+            <ul>
+                <li>
+                    Encargada de maquetación y comunicación con el cliente. 
+                </li>
+                <li>
+                    Decidió cursar el grado de Ingeniería Informática inspirada por el desarrollo y la innovación tecnológica.
+                </li>
+                <li>
+                    Entre sus aficiones destaca todo lo relacionado con el mundo del arte a nivel audiovisual, disfruta pintando sus propios cuadros que vende para recaudar fondos con fines solidarios.
+                </li>
+                <li class="contacto">
+                    Contacto: rebehe01@ucm.es
+                </li>
+            </ul>
         </div>
-        <ul>
-            <li>
-                Encargada de diseño y ayudante de equipo.
-            </li>
-            <li>
-                Experta en redes sociales, es la enargada de todo lo relativo a la publicidad y el contacto con el público.
-            </li>
-            <li>
-                Su principal afición son los animales, resalta su gran conciencia contra el maltrato animal y el cuidado del medioambiente.
-            </li>
-            <li class="contacto">
-                Contacto: mranz02@ucm.es
-            </li>
-        </ul>
+    </div>  
+
+    <div class="container_miembros">
+        <p>Adrián García</p> 
+        <div class="contenido_miembros">
+            <div id="adri" class="perfil">
+                <img src="img/Adri.png" alt="Foto Adri">
+            </div>
+            <ul>
+                <li>
+                    Administrador de recursos y gestor de la base de datos.
+                </li>
+                <li>
+                    Inspirado por el desarrollo profesional de su familia decidió centrar sus estudios en la Ingeniería Informática.
+                </li>
+                <li>
+                    Entre sus aficiones destaca la fotografía, por lo que siempre observa todo desde una perspectiva analítica y detallista.
+                </li>
+                <li class="contacto">
+                    Contacto: adriga10@ucm.es
+                </li >
+            </ul>
+        </div>
     </div>
 
-    <div class="miembro">
-        <div id="rebeca" class="member_photo">
-            <p>Rebeca Herranz</p>
-            <img src="img/Rebeca.jpg" alt="Foto Rebe">
+    <div class="container_miembros">
+        <p>Víctor Santamaría</p> 
+        <div class="contenido_miembros">
+            <div id="victor" class="perfil">
+                <img src="img/Victor.jpg" alt="Foto Víctor">
+            </div>
+            <ul>
+                <li>
+                    Encargado de cartera de proyectos y contacto con el cliente.
+                </li>
+                <li>
+                    Apasionado de la ciberseguridad y la actualidad tecnológica, desarrolla su carrera en este campo.
+                </li>
+                <li>
+                    Su principal afición es la música, de la que no sólo disfruta escuchándola, también compone sus propias canciones.
+                </li>
+                <li class="contacto">
+                    Contacto: vsantama@ucm.es
+                </li>
+            </ul>
         </div>
-        <ul>
-            <li>
-                Encargada de maquetación y comunicación con el cliente. 
-            </li>
-            <li>
-                Decidió cursar el grado de Ingeniería Informática inspirada por el desarrollo y la innovación tecnológica.
-            </li>
-            <li>
-                Entre sus aficiones destaca todo lo relacionado con el mundo del arte a nivel audiovisual, disfruta pintando sus propios cuadros que vende para recaudar fondos con fines solidarios.
-            </li>
-            <li class="contacto">
-                Contacto: rebehe01@ucm.es
-            </li>
-        </ul>
     </div>
 
-    <div class="miembro">
-        <div id="adri" class="member_photo">
-            <p>Adrián García</p> <img src="img/Adri.png" alt="Foto Adri">
+    <div class="container_miembros">
+        <p>Guillermo Ovejero</p> 
+        <div class="contenido_miembros">
+            <div id="guille" class="perfil">
+                <img src="img/Will.png" alt="Foto Wilson">
+            </div>
+            <ul>
+                <li>
+                    Administrador, revisor de propuestas y desarrollador.
+                </li>
+                <li>
+                    Decidió enfocar su carrera a la programación para dispositivos móviles, así como la creación de páginas web.
+                </li>
+                <li>
+                    Entre sus aficiones destaca el deporte, especialmente el baloncesto, lo practica tanto en el papel de jugador, como en el de entrenador.
+                </li>
+                <li class="contacto"> 
+                    Contacto: guilleov@ucm.es
+                </li>
+            </ul>
         </div>
-        <ul>
-            <li>
-                Administrador de recursos y gestor de la base de datos.
-            </li>
-            <li>
-                Inspirado por el desarrollo profesional de su familia decidió centrar sus estudios en la Ingeniería Informática.
-            </li>
-            <li>
-                Entre sus aficiones destaca la fotografía, por lo que siempre observa todo desde una perspectiva analítica y detallista.
-            </li>
-            <li class="contacto">
-                Contacto: adriga10@ucm.es
-            </li >
-        </ul>
     </div>
-
-    <div class="miembro">
-        <div id="victor" class="member_photo">
-            <p>Víctor Santamaría</p> <img src="img/Victor.jpg" alt="Foto Víctor">
-        </div>
-        <ul>
-            <li>
-                Encargado de cartera de proyectos y contacto con el cliente.
-            </li>
-            <li>
-                Apasionado de la ciberseguridad y la actualidad tecnológica, desarrolla su carrera en este campo.
-            </li>
-            <li>
-                Su principal afición es la música, de la que no sólo disfruta escuchándola, también compone sus propias canciones.
-            </li>
-            <li class="contacto">
-                Contacto: vsantama@ucm.es
-            </li>
-        </ul>
-    </div>
-
-    <div class="miembro">
-        <div id="guille" class="member_photo">
-            <p>Guillermo Ovejero</p> <img src="img/Will.png" alt="Foto Wilson">
-        </div>
-        <ul>
-            <li>
-                Administrador, revisor de propuestas y desarrollador.
-            </li>
-            <li>
-                Decidió enfocar su carrera a la programación para dispositivos móviles, así como la creación de páginas web.
-            </li>
-            <li>
-                Entre sus aficiones destaca el deporte, especialmente el baloncesto, lo practica tanto en el papel de jugador, como en el de entrenador.
-            </li>
-            <li class="contacto"> 
-                Contacto: guilleov@ucm.es
-            </li>
-        </ul>
-    </div>
-
-    <div class="miembro">
-        <div id="marlon" class="member_photo">
-            <p>Marlon Campoverde</p>
-            <img src="img/Marlon.png" alt="Foto Marlon">
-        </div>
-        <ul>
-            <li>
-                Responsable de equipo y desarrollador.
-            </li>
-            <li>
-                Su pasión siempre fue el desarrollo hardware, hecho por el cual se enfoca en la Ingeniería de Computadores.
-            </li>
-            <li>
-                Entre sus aficiones destacan el deporte y viajar, siempre dispuesto a descubrir nuevos lugares y enfrentarse a nuevos desafíos.
-            </li>
-            <li class="contacto">
-                Contacto: marlonjc@ucm.es
-            </li>
-        </ul>
-    </div>
-
-
 
 </body>
 
--- app/views/admin.php
+++ app/views/admin.php
@@ -11,28 +11,30 @@
     <?php
         require('./includes/common/cabecera.php');
     ?>    
-        <div id="contenido">
+    
+    <div class="container" id="contenido">
+        <?php
+            if (!isset($_SESSION['admin'])) {
+                echo "<h1>¡Acceso denegado!</h1>";
+                echo "<p>No tienes permisos suficientes para administrar la web.</p>";
+            } else {
+        ?>
+            <h1>Consola de administración</h1>
     <?php
-		if (!isset($_SESSION['admin'])) {
-			echo "<h1>¡Acceso denegado!</h1>";
-			echo "<p>No tienes permisos suficientes para administrar la web.</p>";
-	    } else {
-	?>
-		<h1>Consola de administración</h1>
-	<?php
         }
         //require('./controllers/admin.php'); Si dais el visto bueno borramos este script
         require('./includes/Producto.php');
         require('./includes/Usuario.php');
     ?>
-        
+    <div class="contenido_producto">
+
         <?php
-            echo "<div class='productos'>";
-                if(isset($_POST['nombreUsuario'])) {
-                    $map = Usuario::mostrarUsuariosBuscados();
-                    if ($map !== null) {
-                        foreach ($map as $link => $product_img) {
-                            ?>
+            echo "<div class='productosInicio'>";
+            if(isset($_POST['nombreUsuario'])) {
+                $map = Usuario::mostrarUsuariosBuscados();
+                if ($map !== null) {
+                    foreach ($map as $link => $product_img) {
+                        ?>
                             <a href=<?php echo "'$link'"?>> <img src=<?php echo "'$product_img'"?> alt='imagen'></a>
                             <?php
                         }
@@ -48,33 +50,34 @@
                             <a href=<?php echo "'$link'"?>> <img src=<?php echo "'$product_img'"?> alt='imagen'></a>
                             <?php
                             } 
-                    }else{
-                        echo "<h3> Lo sentimos, ningún producto coincide con su búsqueda.\n </h3>";
-                    }
-                } 
-                else {
-                    echo '</div> 
-                    </div>';   
-                    ?>
+                        }else{
+                            echo "<h3> Lo sentimos, ningún producto coincide con su búsqueda.\n </h3>";
+                        }
+                    } 
+                    else {
+                        echo '</div> 
+                        </div>';   
+                        ?>
 
+</div>
 
-                    <table class='per'>
-                        <tr>
-                            <th class='ini'> Usuarios </th>
-                        </tr>
-                        <tr>
-                        
-                            <th class="buscar">
-                                <form method="POST"> 
-                                    <div class='b'><label> Busca un usuario por su nombre: </label><input type="text" name="nombreUsuario"/>
-                                    <button  type="submit" name="submit_buscarNombre">Buscar</button></div>
-                                </form>
-                            </th>
-
-                        </tr>
-                        <tr>
-                    
-                            <th  class='usuariosInicio'> <?php
+<table class='container'>
+    <tr>
+        <th class='ini'> Usuarios </th>
+    </tr>
+    <tr>
+        
+        <th class="buscar">
+            <form method="POST"> 
+                <div><label> Busca un usuario por su nombre: </label><input type="text" name="nombreUsuario"/>
+                <button class="btn" type="submit" name="submit_buscarNombre">Buscar</button></div>
+            </form>
+        </th>
+        
+    </tr>
+    <tr>
+        
+        <th  class='usuariosInicio'> <?php
                                 echo Usuario::mostrarTodosUsuarios();
                                 ?>
                             </th>                 
@@ -86,8 +89,8 @@
 
                             <th class="buscar">
                                 <form method="POST"> 
-                                    <div class='b'><label> Busca un producto por su nombre: </label><input type="text" name="nombreProducto"/>
-                                    <button  type="submit" name="submit_buscarNombre">Buscar</button></div>
+                                    <div><label> Busca un producto por su nombre: </label><input type="text" name="nombreProducto"/>
+                                    <button class="btn" type="submit" name="submit_buscarNombre">Buscar</button></div>
                                 </form>
                             </th>
                         </tr>
