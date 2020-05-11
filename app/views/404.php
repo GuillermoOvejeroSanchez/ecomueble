e94b7f140b6c2f818e9d432c756002838aac45b7
Marta Ranz González <mranz02@ucm.es>
2020-05-11 11:18:40 +0200

    Pequeños cambios css

--- app/views/404.php
+++ app/views/404.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/default.css">
+    <link rel="stylesheet" href="./new.css">
     <title>404</title>
 </head>
 <body>
